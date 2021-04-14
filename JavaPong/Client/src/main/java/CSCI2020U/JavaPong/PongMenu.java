912e372429f8c2c9be3b830ad0448f43174b4822
Tiseagan Ketharasingam <tiseagan2014@gmail.com>
2021-04-13 20:10:07 -0400

    Ui Progress

--- JavaPong/Client/src/main/java/CSCI2020U/JavaPong/PongMenu.java
+++ JavaPong/Client/src/main/java/CSCI2020U/JavaPong/PongMenu.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
 
 public class PongMenu extends Application 
 {
@@ -22,20 +24,50 @@ public class PongMenu extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        //grid.setHgap(10);
+        grid.setVgap(40);
+
+        Canvas canvas = new Canvas();
+
         //Inits the borderpane root
         BorderPane root = new BorderPane();
         SetBackgroundColor(root, Color.DARKGREY);
+
+        //Set center value to grid
+        root.setCenter(grid);
+        root.setTop(canvas);
 
         //Sets up our primary stage
         primaryStage.setTitle("Main Menu");
         primaryStage.setScene(new Scene(root, 600, 400));
 
-        //Sets the top value to the menu
-        Button playButton = new Button("Play Button");
-        root.setCenter(playButton);
-        root.setAlignment(playButton, Pos.CENTER);
+        
+
+        //Sets the Center value to the menu
+        Button playButton = new Button("Play");
+        grid.add(playButton, 0, 0);
 
         playButton.setOnAction(value ->  {
+            testScene.start(primaryStage); 
+        });
+
+        primaryStage.show();
+
+        Button exitButton =  new Button("Exit");
+        grid.add(exitButton, 0, 1);
+
+        exitButton.setOnAction(value -> {
+            testScene.start(primaryStage);
+        });
+
+        primaryStage.show();
+
+        Button creditsButton =  new Button("Credits");
+        grid.add(creditsButton, 0, 2);
+        
+        creditsButton.setOnAction(value -> {
             testScene.start(primaryStage);
         });
 
---
+++ JavaPong/Client/src/main/java/CSCI2020U/JavaPong/TextGameObject.java
@@ -0,0 +1,87 @@
+package CSCI2020U.JavaPong;
+
+import java.util.List;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
+public class TextGameObject extends GameObject
+{
+    private String _text;
+    private Font _font;
+
+    private Paint _fillColor;
+    private Paint _strokeColor;
+
+    public TextGameObject()
+    {
+        super();
+    }
+
+    public TextGameObject(String text, Font font, Vec2 pos, Vec2 size, Paint fill, Paint stroke)
+    {
+        super(pos, size);
+
+        SetText(text);
+
+        SetFillColor(fill);
+        SetStrokeColor(stroke);
+    }
+
+    @Override
+    public void Draw(GraphicsContext context)
+    {
+        //Overrides base class
+        //Sets fill and stroke before draw
+        context.setFill(_fillColor);
+        context.setStroke(_strokeColor);
+
+        //Gets in world space
+        List<Vec2> posSize = ConvertToWorld(_position, _size);
+
+        context.setFont(f);
+        //Draws the filled rect at position with size
+        context.fillText(_text, posSize.get(0)._x, posSize.get(0)._y, posSize.get(1)._x);
+    }
+
+    protected void SetText(String text)
+    {
+        _text = text;
+    }
+
+    protected void SetFont(Font font)
+    {
+        _font = font;
+    }
+
+    protected void SetFillColor(Paint fill)
+    {
+        _fillColor = fill;
+    }
+
+    protected void SetStrokeColor(Paint stroke)
+    {
+        _strokeColor = stroke;
+    }
+
+    protected String GetText()
+    {
+        return _text;
+    }
+
+    protected Font GetFont()
+    {
+        return _font;
+    }
+
+    protected Paint GetFillColor()
+    {
+        return _fillColor;
+    }
+
+    protected Paint GetStrokeColor()
+    {
+        return _strokeColor;
+    }
+}
